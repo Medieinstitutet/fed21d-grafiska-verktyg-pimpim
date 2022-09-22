138eec81ef203bcd1c02f967b4e07c3ead09b077
Louise Rosenqvist <93682181+lrosenqv@users.noreply.github.com>
2022-09-23 01:27:50 +0200

    header styling done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
   import Header from './components/Header.vue'
-  import TheWelcome from './components/TheWelcome.vue'
-  import SecondaryButton from './components/SecondaryButton.vue'
+  import Main from './components/Main.vue'
 </script>
 
 <template>
@@ -10,10 +9,24 @@
   </header>
 
   <main>
-    <TheWelcome />
+    <Main />
+    <img src="./assets/hero.jpg" alt="city of stockholm"/>
   </main>
+
 </template>
 
 <style scoped>
-  
+  img{
+    width:100vw;
+    position: relative;
+    left:-2rem;
+    margin-top: 40px;
+  }
+
+  main{
+    display: flex;
+    flex-direction: column;
+    gap: 1.4rem;
+    margin: 4rem 0 3rem;
+  }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './assets/main.css'
+import './css/style.css'
 import App from './App.vue'
 
 import { createPinia } from 'pinia'
