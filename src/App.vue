e3e9187dd2d7973ba49d9d90433effaf69b68d7b
Louise Rosenqvist <93682181+lrosenqv@users.noreply.github.com>
2022-09-27 21:43:16 +0200

    info section

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   import Header from './components/Header.vue'
   import Main from './components/Main.vue'
   import Articles from './components/Articles.vue'
+import Info from './components/Info.vue'
 </script>
 
 <template>
@@ -14,8 +15,12 @@
     <img src="./assets/hero.jpg" alt="city of stockholm"/>
   </main>
 
-  <section>
+  <section id="section-news">
     <Articles />
+  </section>
+
+  <section id="section-info">
+    <Info />
   </section>
   
 </template>
