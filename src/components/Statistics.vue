449daadc2f40f28b5d381ad126e76e4fcb18bf19
Louise Rosenqvist <93682181+lrosenqv@users.noreply.github.com>
2022-09-28 19:45:00 +0200

    semantics

---
+++ src/components/Statistics.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  defineProps<{
+    numbers: string
+    object: string
+  }>()
+</script>
+
+<template>
+  <div>
+    <h3>{{ object }}</h3>
+    <p>{{ numbers }}</p>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
